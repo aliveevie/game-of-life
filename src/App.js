cd12b8a4a309ed3f8670a70a7b1a7767847de5d5
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-08-09 11:46:33 +0100

    feat/add Game of life Next generation function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const numRows = 30;
+  const numCols = 30;
+  const [grid, setGrid] = useState([]);
+  
+
+  
+
+
+  // Inside the GameOfLifeGrid component
+
+  useState(() => {
+    const newGrid = Array.from({ length: numRows }, () => 
+      Array.from({ length: numCols }, () => false )
+    );
+    setGrid(newGrid);
+  }, []);  
+
+  function handleClick(rowIndex, colIndex){
+    const newGrid = [...grid]
+
+    newGrid[rowIndex][colIndex] = !newGrid[rowIndex][colIndex]
+    setGrid(newGrid)
+  }
+
+
+  
+  
+  
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <div className="App-header">
+      <div className="grid">
+      {grid.map((row, rowIndex) => (
+        <div className='row' key={rowIndex}>
+          {row.map((cell, colIndex) => (
+            <div
+            key={colIndex}
+            className={`cell ${cell ? 'live' : ''}`}
+            onClick={() => handleClick(rowIndex, colIndex)}
+            ></div>
+          ))}
+        </div>
+      ))}
+      </div>
+  
+
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
